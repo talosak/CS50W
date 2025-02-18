6e8cf509194bf3258012ff56ad2f832500edc6c7
talosak <talosak@hotmail.com>
2025-02-18 13:00:50 +0100

    completed lecture3

---
+++ idk/idk/hello/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = "hello"
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("brian", views.brian, name="brian"),
+    path("david", views.david, name="david"),
+    path("<str:name>", views.greet, name="greet")
+]
