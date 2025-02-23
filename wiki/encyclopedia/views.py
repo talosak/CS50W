f8e3b5f0836a204eab211898a10f637e3d8be85f
talosak <talosak@hotmail.com>
2025-02-23 12:27:04 +0100

    made search bar fully functional

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,38 @@
-from django.shortcuts import render
+import django
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
+import markdown2
 
 
 def index(request):
+    entries = util.list_entries()
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": entries
     })
 
+def entry(request, name):
+    if util.get_entry(name) == None:
+        return render(request, "encyclopedia/invalid.html")
+    title = name
+    content = markdown2.markdown(util.get_entry(name))
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": content
+    })
+
+def search(request):
+    if request.method == "POST":
+        query = request.POST.get("q")
+        results = []
+        if util.get_entry(query) is not None:
+            return redirect("entry", query)
+        for entry in util.list_entries():
+            if query in entry:
+                results.append(entry)
+        return render(request, "encyclopedia/results.html", {
+            "query": query,
+            "results": results,
+        })
