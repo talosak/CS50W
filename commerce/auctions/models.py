ac8b32c3136cedf25b7f01020caadaa4424826ce
talosak <talosak@hotmail.com>
2025-03-06 22:23:26 +0100

    created and migrated models for listings, bids and comments

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,19 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Bid(models.Model):
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    bid = models.ForeignKey(Bid, on_delete=models.CASCADE)
+    image = models.URLField(blank=True)
+    category = models.CharField(max_length=64, blank=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    content = models.TextField()
