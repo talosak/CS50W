9ee0477c4cefe3cb0a5ad44d05465090d93b4785
talosak <talosak@hotmail.com>
2025-03-06 05:59:53 +0100

    started project 2 - commerce (and changed navbar slightly)

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from .models import User
