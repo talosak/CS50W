ffd3de02d842ad3da8f11374178034f674dc169c
talosak <talosak@hotmail.com>
2025-03-08 07:50:24 +0100

    added create url and view

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -61,3 +61,9 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+    
+def create(request):
+    if request.method == "POST":
+        pass
+    else:
+        return render(request, "auctions/create.html")
