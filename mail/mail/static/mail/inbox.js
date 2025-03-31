9da4037ab2fc0a0e8bc66064ebc83e2ec563d662
talosak <talosak@hotmail.com>
2025-03-31 22:31:26 +0200

    added email-view

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -13,6 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
 function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -29,6 +30,8 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'none';
+
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
@@ -41,16 +44,20 @@ function load_mailbox(mailbox) {
     // Render emails
     emails.forEach(email => {
       let div = document.createElement('div');
-      div.innerHTML = `<h4> Email from: ${email.sender}<h4>
-                        <h3> Subject: ${email.subject}<h3>
-                        <h5> Date of creation: ${email.timestamp}`;
+      div.innerHTML = `<h4><div class="row">
+                        <div class="col-3"><strong> ${email.sender}</strong></div>
+                        <div class="col-6 ml-n5"><span> ${email.subject}</span></div>
+                        <div class="col-3 ml-5"><span style="color:gray"> ${email.timestamp}</span></div>
+                        </div></h4>`;
       div.style.border = 'solid 2px #232323';
       div.style.padding = '10px';
+      div.style.cursor = 'pointer';
       if (email.read) {
         div.style.backgroundColor = 'gray';
       } else {
         div.style.backgroundColor = 'white';
       }
+      div.addEventListener('click', () => {view_email(email.id)});
       document.querySelector('#emails-view').append(div);
     });
   });
@@ -73,4 +80,30 @@ function send_email(event) {
 
   // Load user's sent mailbox
   load_mailbox('sent');
+}
+
+function view_email(email_id) {
+  // Get the email via ID
+  fetch(`/emails/${email_id}`)
+  .then(response => response.json())
+  .then(email => {
+    // Load the email's page
+    document.querySelector('#email-view').style.display = 'block';
+    document.querySelector('#compose-view').style.display = 'none';
+    document.querySelector('#emails-view').style.display = 'none';
+
+    document.querySelector('#email-sender').innerHTML = email.sender;
+    document.querySelector('#email-recipients').innerHTML = email.recipients;
+    document.querySelector('#email-subject').innerHTML = email.subject;
+    document.querySelector('#email-timestamp').innerHTML = email.timestamp;
+    document.querySelector('#email-body').innerHTML = email.body;
+  });
+
+  // Mark email as read
+  fetch(`/emails/${email_id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        read: true
+    })
+  }); 
 }
