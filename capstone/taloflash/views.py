341c565d4fb1ddc119a1cabdab2f12524db50788
talosak <talosak@hotmail.com>
2025-05-20 22:43:29 +0200

    started login and register views, pages

--- capstone/taloflash/views.py
+++ capstone/taloflash/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "taloflash/index.html")
+
+def login(request):
+    if request.method == "POST":
+        pass
+    else:
+        return render(request, "taloflash/login.html")
+    
+def register(request):
+    if request.method == "POST":
+        pass
+    else:
+        return render(request, "taloflash/register.html")
