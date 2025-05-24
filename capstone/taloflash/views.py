9814ca6e4523740aca20183691fdac51788d0888
talosak <talosak@hotmail.com>
2025-05-24 08:25:27 +0200

    created register page

--- capstone/taloflash/views.py
+++ capstone/taloflash/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+
+from .models import User, FlashSet, Flashcard, Settings
 
 def index(request):
     return render(request, "taloflash/index.html")
 
-def login(request):
+def login_view(request):
     if request.method == "POST":
         pass
     else:
         return render(request, "taloflash/login.html")
+    
+def logout_view(request):
+    logout(request)
+    return redirect("index")
     
 def register(request):
     if request.method == "POST":
