bf6f879f53aec5eb0e4d6b9d8789f90eecec6b87
talosak <talosak@hotmail.com>
2025-06-15 11:49:01 +0200

    added set creation

--- capstone/taloflash/urls.py
+++ capstone/taloflash/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("createSet", views.createSet, name="createSet"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register_view, name="register"),
--- capstone/taloflash/views.py
+++ capstone/taloflash/views.py
@@ -1,11 +1,28 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.db import IntegrityError
+from django.contrib import messages
 
 from .models import User, FlashSet, Flashcard, Settings
 
 def index(request):
     return render(request, "taloflash/index.html")
+
+def createSet(request):
+    if request.method == "POST":
+        creator = request.user
+        name = request.POST["name"]
+        description = request.POST["description"]
+
+        # Create the set
+        set = FlashSet(creator=creator, name=name, description=description)
+        set.save()
+        set.editors.add(creator)
+        set.save()
+        messages.success(request, "Set created successfully")
+        return redirect("index")
+    else:
+        return render(request, "taloflash/createSet.html")
 
 def login_view(request):
     if request.method == "POST":
@@ -17,16 +34,22 @@ def login_view(request):
         # Check if authentication successful
         if user is not None:
             login(request, user)
+            messages.success(request, "Logged in successfully")
             return redirect("index")
         else:
-            return render(request, "taloflash/login.html", {
-                "message": "Invalid username or password."
-            })
+            messages.error(request, "Invalid username or password")
+            return render(request, "taloflash/login.html")
+    
+    elif request.user.is_authenticated:
+        # User is already logged in
+        messages.error(request, "Already logged in")
+        return redirect("index")
     else:
         return render(request, "taloflash/login.html")
     
 def logout_view(request):
     logout(request)
+    messages.success(request, "Logged out successfully")
     return redirect("index")
     
 def register_view(request):
@@ -37,20 +60,24 @@ def register_view(request):
 
         # Ensure password matches confirmation
         if password != confirmation:
-            return render(request, "taloflash/register.html", {
-                "messageError": "Password must match confirmation"
-            })
+            messages.error(request, "Password must match confirmation")
+            return render(request, "taloflash/register.html")
         
         # Ensure username is unique
         try:
             user = User.objects.create_user(username=username, email="", password=password)
             user.save()
         except IntegrityError:
-            return render(request, "network/register.html", {
-                "message": "Username already taken."
-            })
+            messages.error(request, "Username already taken")
+            return render(request, "network/register.html")
         
         login(request, user)
+        messages.success(request, "Registered successfully")
+        return redirect("index")
+    
+    elif request.user.is_authenticated:
+        # User is already logged in
+        messages.error(request, "Already logged in")
         return redirect("index")
 
     else:
